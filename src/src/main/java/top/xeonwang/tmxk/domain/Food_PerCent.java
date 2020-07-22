18baf7277ea6f37378765da6eeac57bc8be49995
94660530 <94660530@qq.com>
2020-07-22 19:52:12 +0800

    修改了一些错误

--- src/src/main/java/top/xeonwang/tmxk/domain/Food_PerCent.java
+++ src/src/main/java/top/xeonwang/tmxk/domain/Food_PerCent.java
@@ -2,7 +2,7 @@ package top.xeonwang.tmxk.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Food_PerCent
+public class Food_PerCent implements Comparable<Food_PerCent>
 {
 	@JsonProperty("foodname")
 	private String FoodName;
@@ -29,10 +29,12 @@ public class Food_PerCent
 		this.Percent = p;
 	}
 	
+	@Override
 	public int compareTo(Food_PerCent f)
 	{
 		if(this.Percent - f.Percent >= 0)
 			return 1;
 		else return 0;
 	}
+
 }
