61b80818af5781c1808749fba308c00d213d7c42
XeonWangYue <qiwang18@qq.com>
2020-07-22 19:25:27 +0800

    error

--- src/src/main/webapp/js/change.js
+++ src/src/main/webapp/js/change.js
@@ -1,27 +1,8 @@
 function change(result){
 	var chart = $('#container').highcharts();
-
-	var data=[];
-	data.push({
-		name: '12312312',
-		y: 10}
-	);
-	data.push({
-		name: '13463772882',
-		y: 20}
-	);
-	data.push({
-		name: '234534623412',
-		y: 20
-	});
-	data.push({
-		name: '13474',
-		y:15}
-	);
-	data.push({
-		name: '5698',
-		y: 35}
-	);
+	$.ajax({
+		url:
+	})
 	
 	
 	for (var i=0;i<data.length;i++){
